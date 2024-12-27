324da213e46a37c482560c65b84a7c7bf35445c8
ocinpp <ocin25@gmail.com>
2024-12-28 03:14:22 +0800

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="min-h-screen bg-gray-100 py-8">
-    <div class="container mx-auto px-4">
+  <div class="min-h-screen bg-gray-100 py-8 flex flex-col">
+    <div class="container mx-auto px-4 flex-grow">
       <h1 class="text-3xl font-bold text-center mb-8">
         Image Colour Palette Extractor
       </h1>
       <ImageUploader />
     </div>
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import ImageUploader from "./components/ImageUploader.vue";
+import Footer from "./components/Footer.vue";
 </script>
